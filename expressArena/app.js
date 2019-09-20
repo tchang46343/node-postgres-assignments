8e94e03103bb9800c66c34d846c54a55d90ca665
Terrance Chang <tchang46343@gmail.com>
2019-09-19 19:52:28 -0700

    Adding new project and Assignment to github

--- expressArena/app.js
+++ expressArena/app.js
@@ -60,6 +60,65 @@ app.get("/greetings", (req, res) => {
   res.send(greeting);
 });
 
+app.get("/video", (req, res) => {
+  const video = {
+    title: "Cats falling over",
+    description: "15 minutes of cat videos",
+    length: "15.40"
+  };
+  res.json(video);
+});
+
+app.get("/colors", (req, res) => {
+  const colors = [
+    {
+      name: "red",
+      rgb: "FF0000"
+    },
+    {
+      name: "green",
+      rgb: "00FF00"
+    },
+    {
+      name: "blue",
+      rgb: "000FF"
+    }
+  ];
+  res.json(colors);
+});
+
+app.get("/grade", (req, res) => {
+  //get the mark from the query
+  const { mark } = req.query;
+  //do some validation
+  if (!mark) {
+    //mark is required
+    return res.status(400).send("Please provide a mark");
+  }
+
+  const numericMark = parseFloat(mark);
+  if (Number.isNaN(numericMark)) {
+    //mark must be a number
+    return res.status(400).send("makr must be a numeric value");
+  }
+
+  if (numericMark < 0 || numericMark > 100) {
+    //mark must be in range 0 to 100
+    return res.status(400).send("Mark must be in range of 0 to 100");
+  }
+
+  if (numericMark >= 90) {
+    return res.send("A");
+  }
+  if (numericMark > 80) {
+    return res.send("B");
+  }
+  if (numericMark > 70) {
+    return res.send("C");
+  }
+  res.send("F");
+});
+
 app.listen(8000, () => {
   console.log("Express server is listening on port 8000!");
 });
---
+++ express_testing/app.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const morgan = require("morgan");
+
+const app = express();
+
+app.use(morgan("common"));
+
+app.get("/", (req, res) => {
+  res.status(200);
+  res.send("Hello Express!");
+});
+
+module.exports = app;
